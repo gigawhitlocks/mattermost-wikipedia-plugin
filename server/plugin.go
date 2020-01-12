41c5afb070038b353c2e03f23167c4b7cd58e74e
Ian Whitlock <ian@theknown.net>
2020-01-11 20:06:00 -0600

    My changes I want to keep

--- server/plugin.go
+++ server/plugin.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"regexp"
+	"strings"
 	"sync"
 
+	"github.com/mattermost/mattermost-server/model"
 	"github.com/mattermost/mattermost-server/v5/plugin"
 )
+
+// this regexp matches links to Wikipedia pages with or without scheme
+var wikiLinkPattern = regexp.MustCompile(`((http|https):\/\/)?[^\s]+\.wikipedia\.org[^\s]*`)
+
+// this regexp captures the page name and anchor
+// the title is \0 and the anchor is \1
+var wikiPagePattern = regexp.MustCompile(`wiki\/[^\s\#]+(\#[^\s]+)?`)
 
 // Plugin implements the interface expected by the Mattermost server to communicate between the server and plugin processes.
 type Plugin struct {
@@ -20,9 +29,25 @@ type Plugin struct {
 	configuration *configuration
 }
 
-// ServeHTTP demonstrates a plugin that handles HTTP requests by greeting the world.
-func (p *Plugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world!")
+func (p *Plugin) MessageHasBeenPosted(c *plugin.Context, post *model.Post) {
+	for _, link := range wikiLinkPattern.FindAllString(post.Message, -1) {
+		titleAndAnchor := strings.Split(wikiPagePattern.FindStringSubmatch(link)[0], "#")
+
+		var title string
+		var anchor string
+		if len(titleAndAnchor) == 1 {
+			title = titleAndAnchor[0]
+			anchor = ""
+		} else if len(titleAndAnchor) == 2 {
+			title = titleAndAnchor[0]
+			anchor = titleAndAnchor[1]
+		} else {
+			p.API.LogWarn(fmt.Sprintf("Couldn't find title in Wikipedia link %s", link))
+			continue
+		}
+
+		p.API.LogDebug(fmt.Sprintf("%s, %s", title, anchor))
+	}
 }
 
 // See https://developers.mattermost.com/extend/plugins/server/reference/
